d816b5c0ed9a8e7efe71042ad0729d55fdf9ef64
iamdeja <iamdeja@protonmail.ch>
2020-07-03 14:37:47 +0300

    Add ex02.1

---
+++ ch01/ex01.2/ex01.2.go
@@ -0,0 +1,13 @@
+// Ex 1.2 prints the index and value of provided arguments.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for i, arg := range os.Args[1:] {
+		fmt.Println(i, arg)
+	}
+}
