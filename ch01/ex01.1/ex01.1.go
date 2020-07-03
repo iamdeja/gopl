7c83ef84d46958b74423827538dfa99edbd99db0
iamdeja <iamdeja@protonmail.ch>
2020-07-03 14:33:57 +0300

    Add ex01.1

---
+++ ch01/ex01.1/ex01.1.go
@@ -0,0 +1,12 @@
+// Ex 1.1 displays command line arguments and the invoking command.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Join(os.Args[0:], " "))
+}
